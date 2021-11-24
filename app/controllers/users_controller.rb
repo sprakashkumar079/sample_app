4ef70b390a92ad51e7443735728c7f1318cc8f50
sprakashkumar079 <singh.prakashkumar@ongraphmail.com>
2021-11-24 18:15:13 +0530

    adding comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-
+  # creating a new user and also save if valid information is given by user
   def create
     @user = User.new(user_params)
     if @user.save
@@ -18,10 +18,13 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-
+  #user_params method is de-signed to make it visually apparent
+  #which methods are defined after private
   private
     def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                                        :password_confirmation)
+      params.require(:user).permit(:name,
+                                   :email,
+                                   :password,
+                                   :password_confirmation)
     end
 end
