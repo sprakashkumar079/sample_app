fd58627077303a06cb9f674e34c6aef6919690e0
SINGH PRAKASHKUMAR <94169724+sprakashkumar079@users.noreply.github.com>
2021-11-24 18:20:49 +0530

    Finish user signup (#8)
    * Finish user signup
    
    Signup success creates a user in the database and redirects
    to the user show page, and displays a welcome message using
    flash and use integration tests to verify form submission
    behavior and catch regressions.
    
    * adding comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
-  def new
+  #this is show action for call all the users
+  def show
+    @user = User.find(params[:id])
   end
+  #creating a valid new user
+  def new
+    @user = User.new
+  end
+  # creating a new user and also save if valid information is given by user
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+  #user_params method is de-signed to make it visually apparent
+  #which methods are defined after private
+  private
+    def user_params
+      params.require(:user).permit(:name,
+                                   :email,
+                                   :password,
+                                   :password_confirmation)
+    end
 end
