e8b8256dddf52eaded1e939913535894eb2615a0
sprakashkumar079 <singh.prakashkumar@ongraphmail.com>
2021-12-01 11:56:33 +0530

    solve bug
    solve bug in production environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  host = '<nameless-gorge-89102>.herokuapp.com'
+  host = 'nameless-gorge-89102.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = { :address=> 'smtp.sendgrid.net',
                                        :port=> '587',
